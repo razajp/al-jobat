e0e899a425c0159415c542f7e20fc2273ef29207
decorgraphicsstudio <hasanraza03165825495@gmail.com>
2025-03-04 13:31:01 +0500

    Added Authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -18,44 +18,42 @@ class AuthController extends Controller
         return view('auth.login');
     }
 
-    public function loginPost(Request $request) {
-        return "hello world";
+    public function loginPost(Request $request)
+    {
+        if (Auth::check()) {
+            return redirect(route('home'))->with('warning', 'Oops! You’re already loged in. Please logut first!');
+        } else {
+            // Validate the input
+            $request->validate([
+                'username' => 'required|string', // Username field validation
+                'password' => 'required',        // Password field validation
+            ]);
+
+            // Attempt to find the user by username
+            $user = User::where('username', $request->username)->first();
+
+            // Check if user exists and if the password matches the hash
+            if ($user && Hash::check($request->password, $user->password)) {
+                if ($user->status == 'active') {
+                    // Password is correct, login the user
+                    Auth::login($user);
+                    return redirect(route('home'))->with('success', 'Welcome back! You have successfully logged in.'); // Redirect to home
+                } else {
+                    return redirect('/login')->with('error', 'Your account is currently inactive. Please reach out to the administrator for assistance.')
+                        ->withInput($request->only('username'));
+                }
+            }
+
+            // If authentication fails, return error message but do not keep the password in session
+            if (!$user) {
+                return redirect('/login')->withErrors(['username' => 'Invalid username'])
+                    ->withInput($request->only('username'));
+            } else {
+                return redirect('/login')->withErrors(['password' => 'Invalid password'])
+                    ->withInput($request->only('username'));
+            }
+        }
     }
-    // {
-    //     if (Auth::check()) {
-    //         return redirect(route('home'))->with('warning', 'Oops! You’re already loged in. Please logut first!');
-    //     } else {
-    //         // Validate the input
-    //         $request->validate([
-    //             'username' => 'required|string', // Username field validation
-    //             'password' => 'required',        // Password field validation
-    //         ]);
-
-    //         // Attempt to find the user by username
-    //         $user = User::where('username', $request->username)->first();
-
-    //         // Check if user exists and if the password matches the hash
-    //         if ($user && Hash::check($request->password, $user->password)) {
-    //             if ($user->status == 'active') {
-    //                 // Password is correct, login the user
-    //                 Auth::login($user);
-    //                 return redirect(route('home'))->with('success', 'Welcome back! You have successfully logged in.'); // Redirect to home
-    //             } else {
-    //                 return redirect('/login')->with('error', 'Your account is currently inactive. Please reach out to the administrator for assistance.')
-    //                     ->withInput($request->only('username'));
-    //             }
-    //         }
-
-    //         // If authentication fails, return error message but do not keep the password in session
-    //         if (!$user) {
-    //             return redirect('/login')->withErrors(['username' => 'Invalid username'])
-    //                 ->withInput($request->only('username'));
-    //         } else {
-    //             return redirect('/login')->withErrors(['password' => 'Invalid password'])
-    //                 ->withInput($request->only('username'));
-    //         }
-    //     }
-    // }
 
     public function logout(Request $request)
     {
