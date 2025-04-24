639b1f0b3a0f2b125d3bac91870b4881a1b33745
Hasan Raza <hasanraza03165825495@gmail.com>
2025-04-24 19:52:28 +0500

    changes in Search Header

--- resources/views/components/search-header.blade.php
+++ resources/views/components/search-header.blade.php
@@ -11,11 +11,11 @@
         <!-- Search Input -->
         <div class="search-input relative">
             <x-input name="search_box" id="search_box" oninput="searchData(this.value)" placeholder="🔍 Search {{ $heading }}..." withButton btnId="filter-btn" btnClass="dropdown-trigger" btnText='<i class="text-xs fa-solid fa-filter"></i>' />
-            <div class="dropdownMenu text-sm absolute mt-2 top-10 right-0 hidden border border-gray-600 w-48 bg-[var(--h-secondary-bg-color)] text-[var(--text-color)] shadow-lg rounded-xl opacity-0 transform scale-95 transition-all duration-300 ease-in-out z-50">
+            <div class="dropdownMenu text-sm absolute mt-2 top-10 right-0 hidden border border-gray-600 w-48 bg-[var(--h-secondary-bg-color)] text-[var(--text-color)] shadow-lg rounded-2xl opacity-0 transform scale-95 transition-all duration-300 ease-in-out z-50">
                 <ul class="p-2">
                     @foreach ($filter_items as $key => $filter_item)
                         <li>
-                            <label class="flex items-center justify-between cursor-pointer group py-2 px-3 hover:bg-[var(--h-bg-color)] grop2 rounded-md transition-all duration-200 ease-in-out" onclick='setFilter("{{ $key }}")'>
+                            <label class="flex items-center justify-between cursor-pointer group py-2 px-3 hover:bg-[var(--h-bg-color)] grop2 rounded-lg transition-all duration-200 ease-in-out" onclick='setFilter("{{ $key }}")'>
                                 <input type="radio" name="filter" value="{{ $key }}" class="hidden peer" {{ $key == 'all' ? 'checked' : '' }}/>
                                 <span class="transition-all peer-checked:text-[var(--primary-color)] peer-checked:ml-1 grop2-hover:text-[var(--primary-color)]">{{ $filter_item }}</span>
                                 <div class="w-3 h-3 border-2 border-gray-500 rounded-full flex items-center justify-center peer-checked:border-[var(--primary-color)]">
