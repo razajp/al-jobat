08b7ca72b215cebd1b387c983c641dcc715f14dc
office2 <hasanraza03165825495@gmail.com>
2025-07-29 13:36:31 +0500

    33..33..3

--- database/seeders/SetupSeeder.php
+++ database/seeders/SetupSeeder.php
@@ -11,7 +11,7 @@ class SetupSeeder extends Seeder
     {
         $setups = [
             ['short_title' => 'AJG', 'title' => 'Al Jobat',      'type' => 'supplier_category'],
-            ['short_title' => 'CMT', 'title' => 'CMT',           'type' => 'supplier_category'],
+            ['short_title' => 'CMT|S', 'title' => 'CMT|S',           'type' => 'supplier_category'],
             ['short_title' => 'FBR', 'title' => 'Fabric',        'type' => 'supplier_category'],
             ['short_title' => 'FND', 'title' => 'Fund',          'type' => 'supplier_category'],
             ['short_title' => 'GMT', 'title' => 'Garments',      'type' => 'supplier_category'],
@@ -21,16 +21,16 @@ class SetupSeeder extends Seeder
             ['short_title' => 'PLT', 'title' => 'Plot',          'type' => 'supplier_category'],
             ['short_title' => 'PVT', 'title' => 'Private',       'type' => 'supplier_category'],
             ['short_title' => 'SRV', 'title' => 'Services',      'type' => 'supplier_category'],
-            ['short_title' => 'CUT',    'title' => 'Cutting',   'type' => 'worker_type'],
-            ['short_title' => 'DHP',    'title' => 'Dhaap',     'type' => 'worker_type'],
-            ['short_title' => 'CMT',    'title' => 'CMT',       'type' => 'worker_type'],
-            ['short_title' => 'SNG',    'title' => 'Singer',    'type' => 'worker_type'],
-            ['short_title' => 'OFL',    'title' => 'O/F Look',  'type' => 'worker_type'],
-            ['short_title' => 'BTK',    'title' => 'Bartake',   'type' => 'worker_type'],
-            ['short_title' => 'TKN',    'title' => 'Token',     'type' => 'worker_type'],
-            ['short_title' => 'CRP',    'title' => 'Cropping',  'type' => 'worker_type'],
-            ['short_title' => 'PRS',    'title' => 'Press',     'type' => 'worker_type'],
-            ['short_title' => 'PKG',    'title' => 'Packing',   'type' => 'worker_type'],
+            ['short_title' => 'CUT', 'title' => 'Cutting',   'type' => 'worker_type'],
+            ['short_title' => 'DHP', 'title' => 'Dhaap',     'type' => 'worker_type'],
+            ['short_title' => 'CMT', 'title' => 'CMT',       'type' => 'worker_type'],
+            ['short_title' => 'SNG', 'title' => 'Singer',    'type' => 'worker_type'],
+            ['short_title' => 'OFL', 'title' => 'O/F Look',  'type' => 'worker_type'],
+            ['short_title' => 'BTK', 'title' => 'Bartake',   'type' => 'worker_type'],
+            ['short_title' => 'TKN', 'title' => 'Token',     'type' => 'worker_type'],
+            ['short_title' => 'CRP', 'title' => 'Cropping',  'type' => 'worker_type'],
+            ['short_title' => 'PRS', 'title' => 'Press',     'type' => 'worker_type'],
+            ['short_title' => 'PKG', 'title' => 'Packing',   'type' => 'worker_type'],
         ];
 
         foreach ($setups as $setup) {
